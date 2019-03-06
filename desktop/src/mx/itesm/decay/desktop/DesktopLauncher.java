1062f44a78b9e70e8292647eb376a59d924d1fd4
duncanista <30836115+duncanista@users.noreply.github.com>
2019-03-05 21:20:46 -0600

    Initial commit

---
+++ desktop/src/mx/itesm/decay/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package mx.itesm.decay.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import mx.itesm.decay.Decay;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Decay(), config);
+	}
+}
