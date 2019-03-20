5001879d49224d87298cc73655933c4539b164ae
Luis Alfonso Alcántara López Ortega <alcantaraluisalfonso@gmail.com>
2019-03-20 16:08:46 -0600

    Nuevas Clases: MapConverter y Clairo Actualización: GenericCharacter

--- core/src/mx/itesm/decay/Characters/GenericCharacter.java
+++ core/src/mx/itesm/decay/Characters/GenericCharacter.java
@@ -5,6 +5,8 @@ import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.Array;
 
@@ -15,24 +17,29 @@ public abstract class GenericCharacter extends Sprite{
     private int NUM_COLUMNS;
     private int NUM_ROWS;
 
-    // Distances, time, velocity
+    // Distances, time, velocity, density, shape
     private float DX = 4;
     private float DY = 4;
-    private float timer;
 
     // Box2d
     private World world;
+    private Body body;
+    private BodyDef bdef;
 
+    // Animation
     private Texture characterSheet;
-    private Animation<TextureRegion> animation;
+    private Animation<TextureRegion> idleAnimation;
 
 
-    public GenericCharacter(Texture texture, float x, float y){
+    public GenericCharacter(Texture texture, float x, float y, World world){
         sprite = new Sprite(texture);
         sprite.setPosition(x,y);
         characterSheet = texture;
-        animation = createAnimation(4,1);
-        timer = 0;
+        this.world = world;
+        bdef = new BodyDef();
+        bdef.type = BodyDef.BodyType.KinematicBody;
+        bdef.position.set(x,y);
+        body = this.world.createBody(bdef);
 
     }
     public void render(SpriteBatch batch){
@@ -40,27 +47,18 @@ public abstract class GenericCharacter extends Sprite{
     }
 
     // Takes a row from character sheet, add textures regions to an array and returns the created animation
-    public Animation createAnimation(int numColumns, int numRow){
+    public Animation createAnimation(int numColumns, int numRow, float time){
         int lengthRow = characterSheet.getHeight() / NUM_ROWS;
         Array<TextureRegion> regions = new Array<TextureRegion>();
         for(int i = 0; i < numColumns; i++){
             regions.add(new TextureRegion(characterSheet, i*128,(numRow-1)*lengthRow,128,128));
         }
-        Animation<TextureRegion> animation = new Animation<TextureRegion>(0.25f, regions);
+        Animation<TextureRegion> animation = new Animation<TextureRegion>(time, regions);
         return animation;
-    }
-    public float getTimer() {
-        return timer;
     }
 
-    public Animation<TextureRegion> getAnimation() {
-        return animation;
-    }
-    public void setAnimation(Animation<TextureRegion> animation) {
-        this.animation = animation;
-    }
-    public void setTimer(float timer) {
-        this.timer = timer;
+    public Animation<TextureRegion> getIdleAnimation() {
+        return idleAnimation;
     }
     public void setDX(float DX) {
         this.DX = DX;
