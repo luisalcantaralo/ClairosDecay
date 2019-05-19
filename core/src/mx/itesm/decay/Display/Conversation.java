3baa5e1569bb5a3cccc499d28609fc162b8f7805
luisalcantaralo <alcantaraluisalfonso@gmail.com>
2019-05-18 22:41:19 -0500

    Cambios en la estructura

---
+++ core/src/mx/itesm/decay/Display/Conversation.java
@@ -0,0 +1,10 @@
+package mx.itesm.decay.Display;
+
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
+public class Conversation {
+
+    public Conversation(Sprite A, Sprite B, Text[] dialog){
+
+    }
+}
