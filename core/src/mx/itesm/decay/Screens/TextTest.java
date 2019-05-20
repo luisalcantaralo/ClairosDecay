8b610cce499df96378b85f7fa9d735c812c663d8
luisalcantaralo <alcantaraluisalfonso@gmail.com>
2019-05-19 22:46:17 -0500

    Cambios en la estructura

--- core/src/mx/itesm/decay/Screens/TextTest.java
+++ core/src/mx/itesm/decay/Screens/TextTest.java
@@ -1,6 +1,4 @@
 package mx.itesm.decay.Screens;
-
-import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
